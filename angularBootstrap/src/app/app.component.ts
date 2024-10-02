15bd36dae4fc6caaa991c11d1471cd442ef849c3
ionichi <feryandi2303@gmail.com>
2024-10-02 11:15:34 +0700

    feat: angular bootstrap package and some usecase

--- angularBootstrap/src/app/app.component.ts
+++ angularBootstrap/src/app/app.component.ts
@@ -9,5 +9,7 @@ import { RouterOutlet } from "@angular/router";
 	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-	title = "angularBootstrap";
+	name = "Feryandi";
+	nim = "222102476";
+	title = "Angular Bootstrap";
 }
