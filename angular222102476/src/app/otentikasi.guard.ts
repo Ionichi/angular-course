f869dc44c52bdfd070f651411b4a56f8b68fd957
ionichi <feryandi2303@gmail.com>
2024-12-10 19:20:48 +0700

    chore: update angular version to 19.0.3

--- angular222102476/src/app/otentikasi.guard.ts
+++ angular222102476/src/app/otentikasi.guard.ts
@@ -1,5 +1,5 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn } from "@angular/router";
 
 export const otentikasiGuard: CanActivateFn = (route, state) => {
-  return true;
+	return true;
 };
