c09890a03366dabfc6a8317b2eed5538ec5217fa
ionichi <feryandi2303@gmail.com>
2025-01-21 20:20:43 +0700

    style: forex timestamp

--- angular222102476/src/app/forex/forex.component.ts
+++ angular222102476/src/app/forex/forex.component.ts
@@ -66,12 +66,13 @@ export class ForexComponent implements AfterViewInit {
 				(date.getMonth() + 1).toString().padStart(2, "0"),
 				date.getFullYear(),
 			].join("-") +
-			" " +
+			" Pukul " +
 			[
 				date.getHours().toString().padStart(2, "0"),
 				date.getMinutes().toString().padStart(2, "0"),
 				date.getSeconds().toString().padStart(2, "0"),
-			].join(":")
+			].join(":") +
+			" (local)"
 		);
 	}
 
